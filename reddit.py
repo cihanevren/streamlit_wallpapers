ec3d7577c85aa84789929df53084e22b288432af
cihanevren <cihan.evren2@outlook.com>
2022-08-29 14:58:37 +0200

    docstring added

--- reddit.py
+++ reddit.py
@@ -9,6 +9,7 @@ from subreddit_query import subreddit_query
 
 class RedditPost:
     def __init__(self, data):
+
          #assuming that data will come as in this format data['data']['children'][i] so I can iterate over to create RedditPost objects
         required_keywords = ["subreddit","is_self","ups","post_hint","title","downs", \
             "score","url","domain","permalink","created_utc","num_comments","name","over_18"]
@@ -17,31 +18,25 @@ class RedditPost:
         
     
     def __str__(self):
+
         title = self.required_data['title']
         score = self.required_data['score']
         url = self.required_data['url']
         return f"RedditPost -> title : {title}, score : {score}, url : {url}"
-    
-    def download(self):
-        
-        if self.is_downloadable() == True:
-            print("Give it a minute -- It is downloading")
-            todownload = requests.get(self.required_data['url'],allow_redirects = True)
-            
-            with open("wallpapers/" + self.name_image() + ".png", 'wb') as file:
-            
-                file.write(todownload.content)
-                print("One image downloaded.")
-            return f"It is downloaded"
-        
-        else:
-            print("Download failed")
-            
-            return f"download fail"
-    
+
     def is_downloadable(self):
+        """
+        Checks if the RedditPost object is downloadable.
+        Inputs:
+        None
+
+        Outputs:
+        Returns True if it is downloadable
+        Returns False if it is not downloadable
+        
+        """
         url = self.required_data['url']
-        header = requests.head(url,allow_redirects=True)
+        header = requests.head(url, allow_redirects=True)
         if 'Content-Type' in header.headers:
             content_type = header.headers.get('Content-Type')
             if "image" in content_type:
@@ -51,8 +46,46 @@ class RedditPost:
             
         return False
     
-    def name_image(self):
+    def download(self):
+        """
+        Downloads the RedditPost object into current working directory
+        Creates "wallpapers" folder if it is not exists
         
+        Inputs:
+        None
+
+        Outputs:
+        None
+
+        """
+        if self.is_downloadable() == True:
+            print("Give it a minute -- It is downloading")
+            to_download = requests.get(self.required_data['url'], allow_redirects=True)
+            
+            with open("wallpapers/" + self.rename_image() + ".png", 'wb+') as file:
+                file.write(to_download.content)
+                print("One image downloaded.")
+
+            return f"It is downloaded"
+        
+        else:
+            print("Download failed")
+            
+            return f"download fail"
+    
+
+    
+    def rename_image(self):
+        """
+        Rename the images using regex pattern matching
+        
+        Inputs:
+        None
+
+        Outputs:
+        Returns the new image name
+        
+        """
         pattern = re.compile(r'''
             (
                 [\[\(]? \d+ [x×] \d+ [\]\)]?  # like [1920×1080], 1x0, (1x0)
@@ -66,18 +99,29 @@ class RedditPost:
         
         return image_title
 
+    def get_url(self):
+
+        if self.is_downloadable() == True:
+            return self.required_data['url']
+
 def get_urls(reddit_posts):
+
     return [post.required_data['url'] for post in reddit_posts if post.is_downloadable() == True]
 
 def main():
+    query_keyword, total_pages = sys.argv[1:]
     params = None
     urls = []
-    for _ in range(2):
-        json_data = subreddit_query('wallpaper',params)
+    print(total_pages)
+    print(query_keyword)
+    for _ in range(int(total_pages)):
+        json_data = subreddit_query(query_keyword, params)
         reddit_posts = [RedditPost(child) for child in json_data['data']['children']]
 
-        urls.append(get_urls(reddit_posts)) #gets urls for our streamlit app
-        
+        #urls.append(get_urls(reddit_posts)) #gets urls for our streamlit app
+        #for post in reddit_posts:
+
+           # urls.append(post.get_url())
         
     
         for post in reddit_posts:
@@ -88,7 +132,7 @@ def main():
         next_page = json_data['data']['after']
         params = dict(after=next_page)
 
-    return urls
 
 if __name__ == "__main__":
     main()
+    
